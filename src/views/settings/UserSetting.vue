e59da4feb6086b939235c0a55a564f3cb012593a
Ma1akAs <1763611895@qq.com>
2024-03-30 00:54:36 +0800

    用户个人信息设置页面完善

--- src/views/settings/UserSetting.vue
+++ src/views/settings/UserSetting.vue
@@ -3,54 +3,128 @@
 import Card from "@/components/Card.vue";
 import {Message, Refresh, Select, User} from "@element-plus/icons-vue";
 import {useStore} from "@/store/index.js";
-import {computed} from "vue";
+import {computed, reactive, ref} from "vue";
+import {ElMessage} from "element-plus";
+import {post, get} from "@/api";
 
 const store = useStore();
 const registerTime = computed(() => new Date(store.user.registerTime).toLocaleString())
+
+const desc = ref()
+const baseFormRef = ref()
+const emailFormRef = ref()
+
+const baseForm = reactive({
+  username: '',
+  gender: 1,
+  phone: '',
+  qq: '',
+  wx: '',
+  desc: ''
+})
+
+const emailForm = reactive({
+  email: '',
+  code: ''
+})
+
+const validateUsername = (rule, value, callback) => {
+  if (value === '') {
+    callback(new Error('请输入用户名'))
+  } else if(!/^[a-zA-Z0-9\u4e00-\u9fa5]+$/.test(value)){
+    callback(new Error('用户名不能包含特殊字符，只能是中文/英文'))
+  } else {
+    callback()
+  }
+}
+
+const rules = {
+  username: [
+    { validator: validateUsername, trigger: ['blur', 'change'] },
+    { min: 2, max: 8, message: '用户名的长度必须在2-8个字符之间', trigger: ['blur', 'change'] },
+  ],
+  email: [
+    { required: true, message: '请输入邮件地址', trigger: 'blur' },
+    {type: 'email', message: '请输入合法的电子邮件地址', trigger: ['blur', 'change']}
+  ]
+}
+
+const loading = reactive({
+  form: true,
+  base: false
+})
+
+function saveDetails() {
+  baseFormRef.value.validate(isValid => {
+    if(isValid) {
+      loading.base = true;
+      post("/api/user/save-details", baseForm, () => {
+        ElMessage.success("更新用户信息成功");
+        store.user.username = baseForm.username
+        desc.value = baseForm.desc
+        loading.base=false;
+      }, (message) => {
+        ElMessage.warning(message)
+        loading.base = false
+      })
+    }
+  })
+}
+
+get('/api/user/details', data => {
+  baseForm.username = store.user.username
+  baseForm.gender = data.gender
+  baseForm.phone = data.phone
+  baseForm.wx = data.wx
+  baseForm.qq = data.qq
+  baseForm.desc = desc.value = data.desc
+  emailForm.email = store.user.email
+  loading.form = false
+})
 </script>
 
 <template>
   <div style="display: flex">
     <div class="setting-left">
-      <card :icon="User" title="账号信息设置" desc="在这里编辑您的个人信息，您可以在隐私中设置是否选择这些信息">
-        <el-form label-position="top" style="margin: 0 10px 10px 10px;">
-          <el-form-item label="用户名">
-            <el-input />
+      <card :icon="User" title="账号信息设置" desc="在这里编辑您的个人信息，您可以在隐私中设置是否选择这些信息" v-loading="loading.form">
+        <el-form ref="baseFormRef" :model="baseForm" :rules="rules" label-position="top" style="margin: 0 10px 10px 10px;">
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="baseForm.username" maxlength="10"/>
           </el-form-item>
-          <el-form-item label="性别">
-            <el-radio-group>
-              <el-radio label="1">男</el-radio>
-              <el-radio label="2">女</el-radio>
+          <el-form-item label="性别" prop="gender">
+            <el-radio-group v-model="baseForm.gender">
+              <el-radio label="0">男</el-radio>
+              <el-radio label="1">女</el-radio>
             </el-radio-group>
           </el-form-item>
-          <el-form-item label="手机号">
-            <el-input />
+          <el-form-item label="手机号" prop="phone">
+            <el-input v-model="baseForm.phone" maxlength="11"/>
           </el-form-item>
-          <el-form-item label="QQ号">
-            <el-input />
+          <el-form-item label="QQ号" prop="qq">
+            <el-input v-model="baseForm.qq" maxlength="13"/>
           </el-form-item>
-          <el-form-item label="微信号">
-            <el-input />
+          <el-form-item label="微信号" prop="vx">
+            <el-input v-model="baseForm.vx" maxlength="20"/>
           </el-form-item>
-          <el-form-item label="个人简介">
-            <el-input type="textarea" :rows="6"/>
+          <el-form-item label="个人简介" prop="desc">
+            <el-input v-model="baseForm.desc" type="textarea" :rows="6"/>
           </el-form-item>
           <div>
-            <el-button :icon="Select">
+            <el-button @click="saveDetails" :icon="Select" :loading="loading.base">
               保存用户信息
             </el-button>
           </div>
         </el-form>
       </card>
       <card style="margin-top: 10px" :icon="Message" title="电子邮件设置" desc="您可以在这里修改默认绑定的电子邮件地址">
-        <el-form label-position="top" style="margin: 0 10px 10px 10px">
-          <el-form-item label="电子邮件">
-            <el-input/>
+        <el-form ref="emailFormRef" :rules="rules" :model="emailForm" label-position="top" style="margin: 0 10px 10px 10px">
+          <el-form-item label="电子邮件" prop="email">
+            <el-input v-model="emailForm.email"/>
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop="code">
             <el-row style="width: 100%">
               <el-col :span="18">
-                <el-input placeholder="请获取验证码" :gutter="10"/>
+                <el-input placeholder="请获取验证码" :gutter="10" v-model="emailForm.code"/>
               </el-col>
               <el-col :span="6">
                 <el-button type="success" plain style="width: 100%">获取验证码</el-button>
@@ -75,15 +149,7 @@ const registerTime = computed(() => new Date(store.user.registerTime).toLocaleSt
           </div>
           <el-divider style="margin: 10px 0" />
           <div style="font-size: 14px; color: grey; padding: 10px">
-            Flow like lightning. Strike like thunder.
-            No one remembers who the first Arcstrider was. Hardly anyone remembers the Arcstrider at all. Time vanished us like it does memories.
-            But in the darkest days of the Dark Ages, when humanity was utterly defenseless, Arcstriders disciplined their bodies to let the Traveler's energy flow through them, to call lightning itself to hand and wield it like a staff against the Darkness.
-            Become the Lightning, they said.
-            No.
-            Become the staff, they said.
-            No.
-            Become a weapon, they said.
-            We obliged.
+            {{ desc || "该用户尚未填写个人信息" }}
           </div>
         </div>
       </card>
